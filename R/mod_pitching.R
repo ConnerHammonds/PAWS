b747da5bf57a268e1c87e62f9301f082ae1a8430
Conner <connerhammonds@yahoo.com>
2026-01-28 22:02:28 -0600

    updated the server logic to generate the pitcher data filtered by pitcher name

--- R/mod_pitching.R
+++ R/mod_pitching.R
@@ -31,6 +31,31 @@ mod_pitching_ui <- function(id) {
 
 mod_pitching_server <- function(id) {
   moduleServer(id, function(input, output, session) {
-    #server logic
-  })
+
+    # Read the CSV file uploaded by the user
+    csv_data <- reactive({
+      req(input$csv_upload)
+      read.csv(input$csv_upload$datapath, stringsAsFactors = FALSE)
+    })
+
+    # Update the dropdown with unique pitcher names
+    observeEvent(csv_data(), {
+      updateSelectInput(session, "select_pitcher", choices = unique(csv_data()$Pitcher))
+    })
+
+    # Filter data for the selected pitcher
+    filtered_data <- reactive({
+      req(csv_data(), input$select_pitcher)
+      subset(csv_data(), Pitcher == input$select_pitcher)
+    })
+
+    # Render the filtered data table
+    output$pitch_data_table <- renderTable({
+      req(filtered_data())
+      filtered_data()
+    })
+
+})
+
 }
+
